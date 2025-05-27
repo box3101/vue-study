7a07de23b7a1e9fa8a421b0200d24e50d2ccb120
cy.lee <box4101@nate.com>
2025-05-27 23:49:36 +0900

    오늘의 목표 1. 버튼을 누르면 실제 인터넷에서 농담을 가져옴 2. 매번 다른 농담이 나옴

---
+++ pages/day1.vue
@@ -0,0 +1,15 @@
+<template>
+  <H1>Day1 : 진짜짜 API 호출</H1>
+  <button @click="getJoke">농담 가져오기</button>
+  <p>{{ joke }}</p>
+</template>
+
+<script setup>
+const joke = ref("버튼을 눌러서 농담을 들어보세요!.");
+
+const getJoke = async () => {
+    const response = await fetch('https://official-joke-api.appspot.com/random_joke')
+    const data = await response.json()
+    joke.value = data.setup + ' - ' + data.punchline
+};
+</script>
